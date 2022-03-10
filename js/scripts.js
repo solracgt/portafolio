3b0c8763f4da504abb192fe4ddc312b2ac2be139
Carlos López <36755603+solracgt@users.noreply.github.com>
2022-03-09 20:59:53 -0600

    Add files via upload
    Cambio a fondo negro y se agrego botones de abrir o cerrar la pagina a pantalla completa

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,41 @@
+/*var Promise = Element.requestFullscreen(options); */
+
+document.getElementById("Entrar").addEventListener("click", function(e){
+  getFullscreen(document.documentElement);
+},false);
+
+/*var exitPromise = document.exitFullscreen(); */
+
+document.getElementById("Salir").addEventListener("click", function(e){
+  exitFullscreen();
+},false);
+
+function getFullscreen(element){
+  if(element.requestFullscreen) {
+      element.requestFullscreen();
+    } else if(element.mozRequestFullScreen) {
+      element.mozRequestFullScreen();
+    } else if(element.webkitRequestFullscreen) {
+      element.webkitRequestFullscreen();
+    } else if(element.msRequestFullscreen) {
+      element.msRequestFullscreen();
+    }
+}
+
+
+function exitFullscreen() {
+  if(document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if(document.mozCancelFullScreen) {
+    document.mozCancelFullScreen();
+  } else if(document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  }
+}
+
+
+
+
 jQuery('document').ready(function($) {
 	var menuBtn = $('.cab-menu-icon'),
 	    menu = $('.cab-cnt-nav ul');
